8b1cc718e6ba1fe17b699d88f5154d83031a06e9
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-23 19:34:40 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,13 @@
 require_relative "blackjack.rb"
 
-runner
+def runner
+  # code runner here
+  welcome
+  total = initial_round
+  loop do
+    total = hit?(total)
+    display_card_total(total)
+    total > 21 ? break : hit(total)
+  end
+  end_game(total)
+end
